691bb3644eb89bf2fe49d9587f4acd117b9c501b
dnp987 <dnp987@gmail.com>
2025-06-29 22:20:32 -0400

    Added page title verification

--- test3/src/App.java
+++ test3/src/App.java
@@ -4,14 +4,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App {
     public static void main(String[] args) throws Exception {
         String test_sight = "https://www.tsx.com";
-        String page_title = "TMX TSX | TSXV - Toronto Stock Exchange and TSX Venture Exchange";
-        String test_title;
+        String expected_page_title = "TMX TSX | TSXV - Toronto Stock Exchange and TSX Venture Exchange";
+        String actual_page_title;
         System.out.println("Hello, World!");
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
         driver.get(test_sight);
-        test_title = driver.getTitle();
-        System.out.println(test_title);
+        actual_page_title = driver.getTitle();
+        System.out.println(actual_page_title);
+        actual_page_title = driver.getTitle();
+
+        if (actual_page_title.equals(expected_page_title)) {
+            System.out.println("Page title matches the expected page title");
+        } else {
+            System.out.println("Page title does not match the expected_page_title");
+        }
+
         driver.close();
         driver.quit();
     }
