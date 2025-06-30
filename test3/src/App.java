c01881c52ec8d79264dcecb5f1088866b22d1cb6
dnp987 <dnp987@gmail.com>
2025-06-29 23:16:28 -0400

    Added function to check page title

--- test3/src/App.java
+++ test3/src/App.java
@@ -14,14 +14,8 @@ public class App {
         System.out.println(actual_page_title);
         actual_page_title = driver.getTitle();
 
-        /*
-         * if (actual_page_title.equals(expected_page_title)) {
-         * System.out.println("Page title matches the expected page title");
-         * } else {
-         * System.out.println("Page title does not match the expected_page_title");
-         * }
-         */
         check_page_title(expected_page_title, actual_page_title);
+
         driver.close();
         driver.quit();
     }
